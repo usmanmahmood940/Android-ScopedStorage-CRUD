d7064275c33ee36d82105fca8d9f5f1ddef81d4b
Usman <usman.mahmood@brainxtech.com>
2023-05-11 14:29:52 +0500

    Permissions Update and added Save Image Fucntion

--- app/src/main/java/com/example/scopedstorage_crud/MainActivity.kt
+++ app/src/main/java/com/example/scopedstorage_crud/MainActivity.kt
@@ -6,12 +6,14 @@ import android.app.RecoverableSecurityException
 import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.DocumentsContract
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
@@ -23,7 +25,9 @@ import androidx.core.content.ContextCompat
 import com.example.scopedstorage_crud.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,10 +39,14 @@ class MainActivity : AppCompatActivity() {
     private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 2
     private val DELETE_EXTERNAL_STORAGE_REQUEST_CODE = 3
 
+    private val PERMISSION_REQUEST_CODE = 201
+
     // Launcher variables
     private lateinit var pickImage: ActivityResultLauncher<PickVisualMediaRequest>
     private lateinit var pickImageAndDelete: ActivityResultLauncher<PickVisualMediaRequest>
     private lateinit var takePhotoLauncher: ActivityResultLauncher<Void?>
+
+    private var permissionsFlag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,31 +55,33 @@ class MainActivity : AppCompatActivity() {
 
         initLaunchers()
 
+        //
+        if(checkPermissions()){
+            permissionsFlag = true
+        }
+        else{
+            getPermissions()
+        }
+
         binding.btnRead.setOnClickListener {
-            if (checkReadStoragePermission()) {
-                pickImageFromGallery()
-            } else {
-                // Permission not granted, request it
-                getReadPermission()
-            }
+            eventLaunch(this::pickImageFromGallery)
+           // eventLaunch { pickImageFromGallery() }
         }
 
         binding.btnWrite.setOnClickListener {
-            if (checkWriteStoragePermission()) {
-                lauchCamera()
-            } else {
-                // Permission not granted, request it
-                getWritePermission()
-            }
+            eventLaunch(this::lauchCamera)
         }
 
         binding.btnDelete.setOnClickListener {
-            if (checkWriteStoragePermission()) {
-                pickImageForDeletion()
-            } else {
-                // Permission not granted, request it
-                getDeletePermission()
-            }
+            eventLaunch(this::pickImageForDeletion)
+        }
+    }
+    private fun eventLaunch(event:() -> Unit){
+        if (permissionsFlag) {
+            event()
+        }
+        else{
+            getPermissions()
         }
 
 
@@ -86,7 +96,6 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "PhotoPicker , No media selected", Toast.LENGTH_SHORT).show()
             }
         }
-
         pickImageAndDelete =
             registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
                 if (uri != null) {
@@ -99,7 +108,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-
         // Camera activity Launcher
         takePhotoLauncher =
             registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
@@ -108,9 +116,6 @@ class MainActivity : AppCompatActivity() {
                     saveImageToExternalStorage(bitmap)
                 }
             }
-
-
-
     }
 
     private fun pickImageFromGallery() {
@@ -126,34 +131,39 @@ class MainActivity : AppCompatActivity() {
 
     //Save Image to External Storage
     private fun saveImageToExternalStorage(bitmap: Bitmap) {
-        val imageCollection =
-            if (SDK_28_ABOVE) MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-            else MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        if(SDK_28_ABOVE) {
+            val imageCollection =
+                if (SDK_28_ABOVE) MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+                else MediaStore.Images.Media.EXTERNAL_CONTENT_URI
 
-        val fileName = "my_image_${System.currentTimeMillis()}.jpg"
+            val fileName = "my_image_${System.currentTimeMillis()}.jpg"
 
-        val contentValues = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, "$fileName.jpg")
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(
-                MediaStore.Images.Media.RELATIVE_PATH,
-                Environment.DIRECTORY_PICTURES + File.separator + "Test Storage"
-            )
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, "$fileName.jpg")
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.Images.Media.RELATIVE_PATH,
+                    Environment.DIRECTORY_PICTURES + File.separator + "Test Storage"
+                )
 
-        }
-        try {
-            contentResolver.insert(imageCollection, contentValues)?.also { uri ->
-                contentResolver.openOutputStream(uri).use { outputStream ->
-                    if (!bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
-                        throw IOException("Couldn't save bitmap")
+            }
+            try {
+                contentResolver.insert(imageCollection, contentValues)?.also { uri ->
+                    contentResolver.openOutputStream(uri).use { outputStream ->
+                        if (!bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
+                            throw IOException("Couldn't save bitmap")
+                        }
                     }
-                }
-            } ?: throw IOException("Couldn't create MediaStore entry")
-            Toast.makeText(this, "Image Saved", Toast.LENGTH_SHORT).show()
+                } ?: throw IOException("Couldn't create MediaStore entry")
+                Toast.makeText(this, "Image Saved", Toast.LENGTH_SHORT).show()
 
-        } catch (e: IOException) {
-            e.printStackTrace()
-            Toast.makeText(this, "Unable to Save Image", Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Toast.makeText(this, "Unable to Save Image", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else{
+            saveImageToStorageForOldVersion(bitmap)
         }
     }
     @SuppressLint("Range")
@@ -172,68 +182,69 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-
     // Permissions Methods
-    private fun getReadPermission() {
+    private fun getPermissions(){
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-            READ_EXTERNAL_STORAGE_REQUEST_CODE
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
         )
     }
-    private fun getWritePermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-            WRITE_EXTERNAL_STORAGE_REQUEST_CODE
-        )
-    }
-    private fun getDeletePermission(){
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-            DELETE_EXTERNAL_STORAGE_REQUEST_CODE
-        )
-    }
-
-    private fun checkReadStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
+    private fun checkPermissions(): Boolean {
+        val readPermission =  ContextCompat.checkSelfPermission(
             this, Manifest.permission.READ_EXTERNAL_STORAGE
-        ) != PackageManager.PERMISSION_GRANTED
-    }
-    private fun checkWriteStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
+        ) == PackageManager.PERMISSION_GRANTED
+        val writePermission = ContextCompat.checkSelfPermission(
             this, Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) != PackageManager.PERMISSION_GRANTED
+        ) == PackageManager.PERMISSION_GRANTED
+        val check = readPermission && writePermission
+       return check
     }
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == READ_EXTERNAL_STORAGE_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, proceed to pick image from gallery
-                pickImageFromGallery()
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResults.size > 1 ) {
+                val READ_EXTERNAL_STORAGE = grantResults[0] === PackageManager.PERMISSION_GRANTED
+                val WRITE_EXTERNAL_STORAGE = grantResults[1] === PackageManager.PERMISSION_GRANTED
+                if(READ_EXTERNAL_STORAGE && WRITE_EXTERNAL_STORAGE){
+                    permissionsFlag = true
+                }
+                else {
+                    Toast.makeText(this, "Please Give Storage Persmission", Toast.LENGTH_SHORT).show()
+                }
             } else {
-                Toast.makeText(this, "Unable to Load Image", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "size is smaller than 1", Toast.LENGTH_SHORT).show()
             }
         }
 
-        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                lauchCamera()
-            } else {
-                Toast.makeText(this, "Please Give Storage Persmission", Toast.LENGTH_SHORT).show()
-            }
+    }
+    private fun saveImageToStorageForOldVersion(bitmap: Bitmap){
+        // If sdk is below 28
+        val root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            .toString()
+        val myDir = File("$root/saved_images")
+        myDir.mkdirs()
+        val generator = Random()
+        var n = 10000
+        n = generator.nextInt(n)
+        val fname = "Image-$n.jpg"
+        val file = File(myDir, fname)
+        if (file.exists()) file.delete()
+        try {
+            val out = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
+            out.flush()
+            out.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
         }
-        if (requestCode == DELETE_EXTERNAL_STORAGE_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                pickImageForDeletion()
-            } else {
-                Toast.makeText(this, "Please Give Storage Persmission", Toast.LENGTH_SHORT).show()
-            }
+        MediaScannerConnection.scanFile(
+            this, arrayOf<String>(file.toString()), null
+        ) { path, uri ->
+            Log.i("ExternalStorage", "Scanned $path:")
+            Log.i("ExternalStorage", "-> uri=$uri")
         }
     }
-
 
 }
